46f0e54f8493551dbb5ef00081d950a0117762a0
AlexeyYankovoy <107897850+AlexeyYankovoy@users.noreply.github.com>
2022-06-21 04:39:51 +0300

    Delete src/store directory

--- src/store/reducers/authReducer.js
+++
@@ -1,6 +0,0 @@
-import { firebaseConfig } from "../../firebase_/firebase_conf"
-import { initializeApp } from 'firebase/app'
-
-export const authReducer = () => {
-    return initializeApp(firebaseConfig)
-}
