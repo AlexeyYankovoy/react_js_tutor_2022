6c23f1e9534b768af3b323a449b55600668daaea
AlexeyYankovoy <107897850+AlexeyYankovoy@users.noreply.github.com>
2022-06-21 05:02:18 +0300

    Update Todo.js

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,115 +1,83 @@
-import { Button, Checkbox, List, Col, Input, Spin } from 'antd';
-import React, {Component, useContext, useEffect, useMemo, useRef, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { useMyHookTest } from '../../hooks/useMyHookTest';
-import { store } from '../../store';
-import { fetchTodos } from '../../store/actions/todo';
+import { Button, Checkbox, List, Col, Input, Pagination } from 'antd';
+import React, {useState, useEffect} from 'react';
+import { useFetchData } from '../../hooks/useMyHookTest';
 
 import classes from './Todo.module.css'
-export const Todo = () =>  {
 
-
-    const [todoList,setTodoList] = useState([])
-    const [currentPage,setCurrentPage] = useState(1)
-    const [perPage,setPerPage] = useState(10)
-    const dispatch = useDispatch();
-
-    useEffect(()=>{
-        dispatch(fetchTodos)
-    },[dispatch])
-
+function TodoList(props) {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [perPage, setPerPage] = useState(10);
+    const [list, setList] = useState([]);
+    var todos = useFetchData('todos');
     
-    const handelerTodoChecked = (todoID) =>{
-        let todo = JSON.parse(JSON.stringify(this.state.todoList))
-
-        todo = todo.filter((item)=>{
-            if(item.id == todoID){
-                item.completed = !item.completed
+    useEffect(() => {
+        setList(todos);
+    }, [todos])
+    
+    function handelerTodoChecked(todoID) {
+        let todo = JSON.parse(JSON.stringify(list));
+        todo = todo.filter((item) => {
+            if (item.id === todoID) {
+                item.completed = !item.completed;
             }
             return item;
         })
-        setTodoList(todo)
+        setList(todo);
     }
 
-    const handlerDeleteTodo = (todoID) => {
-        let todo = JSON.parse(JSON.stringify(this.state.todoList))
-
-        todo = todo.filter((item)=>{
-            if(item.id != todoID) return item;
-            
+    function handlerDeleteTodo(todoID) {
+        let todo = JSON.parse(JSON.stringify(list));
+        todo = todo.filter((item) => {
+            if (item.id !== todoID) return item;
+            else return undefined;
         })
-        setTodoList(todo)
+        setList(todo);
     }
 
-    const handlerAddToTodo =(e) => {
-        console.log(e)
-        let defVariable = [{id: Math.random(), title:e.target.value, completed: false}]
-        const data = [...todoList, ...defVariable]
-        setTodoList(data)
+    function handlerAddToTodo(e) {
+        console.log(e);
+        let defVariable = [{id: Math.random(), title:e.target.value, completed: false}];
+        setList([...list, ...defVariable]);
     }
-        const customStyle = {
-            buttonBack: {
-                background: 'red',
-                backgroundColor: 'red'
-            },
 
-        }
-        
-        const todos = useSelector(state => state.todos)
-        console.log('todos',todos)
-        if(todos?.loading){
-            // dispatch(fetchTodos)
-            return <Spin>12</Spin>
-        }else{
-            return(
-            <>
-                {/* <Home /> */}
-                <Col span={8} style={{margin: '0 auto'}}>
-                    <h3>My todo list</h3>
-                    <List>
-                        <Input placeholder='Добавить todo в список' onPressEnter={(e) => this.handlerAddToTodo(e)}/>
-                        {todos.todo.map((item,index)=>{
-                            return(
-                                    
-                                    <List.Item key={index} style={{listStyle: 'decimal', textDecoration : item.completed ? 'line-through' : 'none'}} >
-                                        <Checkbox onChange={() => handelerTodoChecked(item.id) } checked={item.completed ? true : false }/>
-                                        {item.title}
-                                        <Button className={classes.button} onClick={()=> handlerDeleteTodo(item.id) } >Удалить</Button>
-                                    </List.Item>
-                            )
-                        })}
-                    </List>
-                </Col>
-            </>
-        )
+    function handlerUpdatePages (page, pageSize) {
+        setCurrentPage(page ? page : 1);
+        setPerPage(pageSize);
     }
-}
 
-
-const Home = () =>{
-
-    const [count,setCount] = useState(0)
-
-    const text = useMyHookTest(123)
-    useEffect(()=>{
-        console.log('did mount ')
-
-        return;
-    },[])
-
-    // useContext();
-    // useMemo()
-    const inputRef = useRef()
-
-    const handeIncrementCount = ()=>{
-        setCount(count+1);
-    }
-    return(
+    if (list !== undefined) {
+        let showList = (props.uid !== undefined) ? list.filter((item) => item.userId == props.uid) : list;
+        let firstItem = (currentPage - 1) * perPage;
+        let lastItem = currentPage * perPage;
+        let pageList = showList.slice(firstItem, lastItem);
+    
+        return (
         <>
-            <p>Home component {text}</p>
-            <input type="text" placeholder='enter value' ref={inputRef} onKeyUp={()=>console.log(inputRef.current.value)}/>
-            <input type="button" value={'++'} onClick={()=>handeIncrementCount()}/>
+            <Col span={8} style={{margin: '0 auto'}}>
+                <h3>Todo list {props.uid ? '#' + props.uid : ''}</h3>
+                <List>
+                    <Input placeholder='Добавить todo в список' onPressEnter={(e) => handlerAddToTodo(e)}/>
+                    {pageList.map((item, index)=>{
+                        return(          
+                            <List.Item key={index} style={{listStyle: 'decimal', textDecoration : item.completed ? 'line-through' : 'none', display: 'flex'}} >
+                                <div>
+                                    <Checkbox onChange={() => handelerTodoChecked(item.id) } checked={item.completed ? true : false } style={{marginRight: 16}}/>
+                                    <span>{item.title}</span>
+                                </div>
+                                <Button className={classes.button} onClick={()=> handlerDeleteTodo(item.id) }>Удалить</Button>
+                            </List.Item>
+                        )
+                    })}
+                </List>
+                <Pagination size="small" showSizeChanger showQuickJumper defaultCurrent={currentPage} defaultPageSize={perPage} total={showList.length} onChange={handlerUpdatePages} style={{paddingBottom: 16}} />
+            </Col>
         </>
-    )
+        );
+    }
+    else {
+        return(<></>);
+    }
+    
 }
+
+export default TodoList;
