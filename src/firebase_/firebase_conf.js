87629b6e7de9727f6d4480f5cbb03573b0338085
AlexeyYankovoy <107897850+AlexeyYankovoy@users.noreply.github.com>
2022-06-21 04:38:25 +0300

    Delete src/firebase_ directory

--- src/firebase_/firebase_conf.js
+++
@@ -1,5 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "",                             // Auth / General Use
-    authDomain: "-5617c.firebaseapp.com",         // Auth with popup/redirect
-    projectID: "-5617c"
-}
